96750edab83677c19cee93860a0ed7e7c4d38865
JeanRemiDelteil <jeanremi.delteil@gmail.com>
2019-06-28 16:35:51 +0200

    use superstatic to serve project locally (same as `firebase serve --only hosting` but without firebase dependency)

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -5,6 +5,7 @@ import runCmd from './plugin/rollup-plugin-run-command';
 import {terser} from 'rollup-plugin-terser';
 import jsonPlugin from 'rollup-plugin-json';
 import virtualModule from './plugin/rollup-plugin-virtual-processed';
+import copy from 'rollup-plugin-copy';
 
 import configFromSource from '../src/main/config.json';
 
@@ -33,47 +34,67 @@ async function getRollUpConfig() {
 	await new Promise(res => rimraf(`./${config.outputFolder}/**`, res));
 	
 	
-	return rollUpConfigs.map(rollUpConfig => ({
-		...rollUpConfig,
+	return rollUpConfigs.map((rollUpConfig, index) => {
 		
-		'plugins': [
-			// Import config from build config
-			virtualModule({
-				'src/main/config.json': JSON.stringify(srcConfig),
-			}),
+		const modernBuild = index === 0;
+		
+		return {
+			...rollUpConfig,
 			
-			...rollUpConfig.plugins.filter(plugin => !/^terser$/.test(plugin.name)),
-			
-			// Resolve commonJS modules
-			commonjs(),
-			
-			jsonPlugin({
-				exclude: ['node_modules/**'],
-				preferConst: true,
-			}),
-			
-			// Minification
-			production && terser({
-				mangle: {
-					properties: {
-						regex: /^_/,
+			'plugins': [
+				// Import config from build config
+				virtualModule({
+					'src/main/config.json': JSON.stringify(srcConfig),
+				}),
+				
+				...rollUpConfig.plugins.filter(plugin => !/^terser$/.test(plugin.name)),
+				
+				// Resolve commonJS modules
+				commonjs(),
+				
+				jsonPlugin({
+					exclude: ['node_modules/**'],
+					preferConst: true,
+				}),
+				
+				// Only copy files for first build
+				modernBuild && copy({
+					targets: [
+						{
+							src: [
+								'src/firebase.json',
+								'src/.firebaserc',
+							],
+							dest: `./${config.outputFolder}`,
+						},
+					],
+					copyOnce: true,
+					verbose: true,
+				}),
+				
+				// Minification
+				production && terser({
+					mangle: {
+						properties: {
+							regex: /^_/,
+						},
 					},
-				},
-			}),
+				}),
+				
+				serve && runCmd({
+					cmd: `cd ./${config.outputFolder} && superstatic --port 5000 --host localhost`,
+					runOnce: true,
+				}),
+			],
 			
-			serve && runCmd({
-				cmd: `http-server ./build/src/dev/`,
-				runOnce: true,
-			}),
-		],
-		
-		'onwarn'(warning, rollupWarn) {
-			// Shush CIRCULAR_DEPENDENCY for node_modules only
-			if (warning.code === 'CIRCULAR_DEPENDENCY' && /^node_modules[\\/]/.test(warning['importer'])) return;
-			
-			rollupWarn(warning);
-		},
-	}));
+			'onwarn'(warning, rollupWarn) {
+				// Shush CIRCULAR_DEPENDENCY for node_modules only
+				if (warning.code === 'CIRCULAR_DEPENDENCY' && /^node_modules[\\/]/.test(warning['importer'])) return;
+				
+				rollupWarn(warning);
+			},
+		};
+	});
 }
 
 
