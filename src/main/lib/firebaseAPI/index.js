2b368dd42c650f9990dd43bbe37f949a2c97692e
JeanRemiDelteil <jeanremi.delteil@gmail.com>
2019-09-07 16:10:41 +0200

    Fix annoying issue with webpack and firebase app: clear all existing app (there should NOT existe any though) before creating the default app

--- src/main/lib/firebaseAPI/index.js
+++ src/main/lib/firebaseAPI/index.js
@@ -1,11 +1,15 @@
 import {firebaseConfig} from '../../config.json';
 import {driver} from './firebaseDatabaseDriver';
 
+// Clear existing firebase apps
+// noinspection TypeScriptUMDGlobal
+firebase.apps.length && firebase.apps.forEach(app => app.delete());
+
 // noinspection TypeScriptUMDGlobal, ES6ModulesDependencies
 /**
  * @type {firebase.app.App}
  */
-export const app = firebase.initializeApp(firebaseConfig);
+export const app = firebase.initializeApp(firebaseConfig, new Date().toISOString());
 
 export const ref = app.database().ref();
 
